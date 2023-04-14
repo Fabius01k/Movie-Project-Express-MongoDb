678b64e6de14ffcce1cb14762afd5a3962a16939
Fabius01k <pashamuruskinn@gmail.com>
2023-04-14 12:29:52 +0300

    13/04
    my homework is not correct
    but i did again

--- src/validadation/post-validation.ts
+++ src/validadation/post-validation.ts
@@ -7,30 +7,33 @@ export const postCreateValidators = [
         if (!blog) {
             throw new Error('Blog ID is not valid');
         }
-        return true }),
-    body('title').isString().notEmpty().
-    trim().isLength({min:1,max:30}).
-    withMessage('title is not correct'),
+        return true
+    }),
+    body('title').isString().notEmpty().trim().isLength({min: 1, max: 30}).withMessage('title is not correct'),
 
-    body('shortDescription').isString().notEmpty().
-    trim().isLength({min:1,max:100}).
-    withMessage('shortDescription is not correct'),
+    body('shortDescription').isString().notEmpty().trim().isLength({
+        min: 1,
+        max: 100
+    }).withMessage('shortDescription is not correct'),
 
-    body('content').isString().notEmpty().
-    trim().isLength({min:1,max:1000}).
-    withMessage('content is not correct'),
+    body('content').isString().notEmpty().trim().isLength({min: 1, max: 1000}).withMessage('content is not correct'),
 ]
 
 export const postUpdateValodators = [
-    body('title').isString().notEmpty().
-    trim().isLength({min:1,max:30}).
-    withMessage('title is not correct'),
+    body('title').isString().notEmpty().trim().isLength({min: 1, max: 30}).withMessage('title is not correct'),
 
-    body('shortDescription').isString().notEmpty().
-    trim().isLength({min:1,max:100}).
-    withMessage('shortDescription is not correct'),
+    body('shortDescription').isString().notEmpty().trim().isLength({
+        min: 1,
+        max: 100
+    }).withMessage('shortDescription is not correct'),
 
-    body('content').isString().notEmpty().
-    trim().isLength({min:1,max:1000}).
-    withMessage('content is not correct'),
+    body('content').isString().notEmpty().trim().isLength({min: 1, max: 1000}).withMessage('content is not correct'),
+
+    body('blogId').isString().notEmpty().custom((value: string) => {
+        const blog = blogs.find(blog => blog.id === value)
+        if (!blog) {
+            throw new Error('Blog ID is not valid');
+        }
+        return true
+    }),
 ]
